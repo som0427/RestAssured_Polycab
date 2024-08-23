95f3cf9f00740a0319191abe1b0589a9ac3c86d7
som0427 <csoumya0427@gmail.com>
2024-08-23 07:51:00 +0530

    4th commit

--- target/timeline/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"ce2b8b23-dd8f-484f-afb2-612aa50e84c8","feature":"Implement OTP Functionality with Security Measures for mobile app login. Validate rate limiting, anti-automation checks,","scenario":"Controlled OTP Resend Interval","start":1724367911306,"group":1,"content":"","tags":"@loginmobileappvaptsc1,","end":1724367915545,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
